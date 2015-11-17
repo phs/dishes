6b32485a61c025c465f5be8cf2ef4e7df4279783
Phil Smith <phil.h.smith@gmail.com>
2015-11-17 12:04:59 -0800

    bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email  'phil.h.smith@gmail.com'
 license           'MIT'
 description       'Cleans up recipes after removal'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.1.1'
